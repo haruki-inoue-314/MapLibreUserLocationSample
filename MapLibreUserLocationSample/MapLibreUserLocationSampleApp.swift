2658e50a42f86c406cf20aef982d87c53fddbc00
HarukiInoue <aitk15015kk@outlook.com>
2024-04-14 22:29:17 +0900

    Initial Commit

---
+++ MapLibreUserLocationSample/MapLibreUserLocationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLibreUserLocationSampleApp.swift
+//  MapLibreUserLocationSample
+//
+//  Created by 井上晴稀 on 2024/04/14.
+//
+
+import SwiftUI
+
+@main
+struct MapLibreUserLocationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
